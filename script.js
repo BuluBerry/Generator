f90f9c2c324748a3cb992ce39d2b05c435319ed9
Buluberry <jessibolser@gmail.com>
2022-10-19 13:30:03 -0500

    added race, class, background array

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+Functions Needed:
+1. Race
+2. Class
+3. Background
+*/
+const race = ['Dragonborn', 'Dwarf', 'Elf', 'Gnome', 'Half-Elf', 'Halfling','Half-Orc','Human']
+const classes = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rouge', 'Sorcerer', 'Warlock', 'Wizard']
+const background = ['Acolyte', 'Charlatan', 'Criminal', 'Entertainer', 'Folk Hero', 'Guild Artisan', 'Hermit', 'Noble', 'Outlander', 'Sage', 'Sailor', 'Soldier', 'Urchin',]
