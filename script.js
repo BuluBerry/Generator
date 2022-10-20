1d89db479a7872671cac3eabbc8ea6f0410747e0
Buluberry <jessibolser@gmail.com>
2022-10-19 23:26:51 -0500

    added a function

--- script.js
+++ script.js
@@ -4,14 +4,25 @@ Functions Needed:
 2. Class
 3. Background
 */
+
 const race = ['Dragonborn', 'Dwarf', 'Elf', 'Gnome', 'Half-Elf', 'Halfling','Half-Orc','Human']
 const classes = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rouge', 'Sorcerer', 'Warlock', 'Wizard']
 const background = ['Acolyte', 'Charlatan', 'Criminal', 'Entertainer', 'Folk Hero', 'Guild Artisan', 'Hermit', 'Noble', 'Outlander', 'Sage', 'Sailor', 'Soldier', 'Urchin',]
 
-let randomRace = Math.floor(Math.random() * (race.length));
+function generateRandomItem(arr, itemName) {
+    let randomItem = Math.floor(Math.random() * (arr.length));
+    console.log(`Your ${itemName} will be ${arr[randomItem]}`);
+}
+
+
+generateRandomItem(race, 'race');
+generateRandomItem(classes, 'class');
+generateRandomItem(background, 'background')
+
+/*let randomRace= Math.floor(Math.random() * (race.length));
 let randomClass = Math.floor(Math.random() * (classes.length));
 let randomBackground = Math.floor(Math.random() * (background.length));
 
 console.log(`Your race will be ${race[randomRace]}.`);
 console.log(`Your class is ${classes[randomClass]}`);
-console.log(`Your background will be ${background[randomBackground]}`);
+console.log(`Your background will be ${background[randomBackground]}`);*/
